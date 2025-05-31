3739bd7baf1f201c1143b2cbb0eed231fe87115e
rajab <rmmehedi007@gmail.com>
2025-05-31 22:26:18 +0600

    Data type-1

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-print("Hellow World!")
-x=10
-y=20
-sum=x+y
-sub=x-y
-mul=x*y
-div=x/y
+#1.Numeric data type
 
-#This is comment
+Age=20  #Int data type
+print(Age)
 
-"""
-multiple comment
-"""
+Marks= 81.7 #Float data type
+print(Marks)
 
-print(sum)
-print(sub)
-print(mul)
-print(div)
+complex_number= 5+2j #complex data type
+print(complex_number)
+
+#2.String data type
+
+name= "Rajab Mohammad Mehedi" #String data type
+print(name)
+
+#3.Sequence data type
+
+#3.1 List data type
+Club=["Barcelona","Liverpool","PSG","Inter"]
+print(Club)
+
+Latter=["A","B","C","D","E","F","G"]
+print(Latter[5])
+
+Mixed=["Barcelona","200","30.5","9+5Y"]
+print(Mixed)
+
+#3.2 Tuple data type
+#this id simmilar to list but imutable it is
+
+#3.2 Range data type
+
+numbers= range(0,20)
+print(*numbers)
+
+numbers1= range(0,20,3)
+print(*numbers1)
+
+numbers2= range(0,20,4)
+print(*numbers2)
